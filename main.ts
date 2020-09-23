87f9a74f0f30bd6799058dd2c9f813aa45da3dbc
JosephLaSorsa <71726985+JosephLaSorsa@users.noreply.github.com>
2020-09-23 14:40:59 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,44 @@
+input.onButtonPressed(Button.A, function () {
+    Player = randint(1, 5)
+    basic.showString("" + (Player))
+    basic.pause(1)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    if (Player == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Player == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Player == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+let Player = 0
+Player = 0
 basic.forever(function () {
 	
 })
