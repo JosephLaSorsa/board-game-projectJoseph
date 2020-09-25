7ab37f41c4c1c9e0a86e8c590c56585b2c30b550
JosephLaSorsa <71726985+JosephLaSorsa@users.noreply.github.com>
2020-09-25 13:43:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,66 @@
 input.onButtonPressed(Button.A, function () {
-    Player = randint(1, 5)
+    Player = Player + randint(1, 5)
     basic.showString("" + (Player))
-    basic.pause(1)
+    basic.pause(1000)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     if (Player == 1) {
         basic.showLeds(`
-            . . . . .
-            . . . . .
             # . . . .
-            . . . . .
-            . . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
             `)
+        basic.pause(2000)
+        basic.clearScreen()
     } else if (Player == 2) {
         basic.showLeds(`
-            . . . . .
-            . . . . .
             . # . . .
-            . . . . .
-            . . . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
             `)
+        basic.pause(2000)
+        basic.clearScreen()
     } else if (Player == 3) {
         basic.showLeds(`
-            . . . . .
-            . . . . .
             . . # . .
-            . . . . .
-            . . . . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
             `)
-    } else if (false) {
-    	
-    } else if (false) {
-    	
+        basic.pause(2000)
+        basic.clearScreen()
+    } else if (Player == 4) {
+        basic.showLeds(`
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . # .
+            `)
+        basic.pause(2000)
+        basic.clearScreen()
+    } else if (Player == 5) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        basic.pause(2000)
+        basic.clearScreen()
     } else {
     	
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    Player = 0
 })
 let Player = 0
 Player = 0
