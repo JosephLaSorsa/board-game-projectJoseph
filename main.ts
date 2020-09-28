27531882bb626f1211bf89cce00aa3c993a97193
JosephLaSorsa <71726985+JosephLaSorsa@users.noreply.github.com>
2020-09-28 15:16:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     Player = Player + randint(1, 6)
     basic.showString("" + (Player))
-    basic.pause(1000)
+    basic.pause(2000)
     basic.clearScreen()
+    if (Enemy == Player) {
+        Enemy += -1
+        basic.showIcon(IconNames.Angry)
+        basic.pause(500)
+        basic.clearScreen()
+        basic.showString("You got attacked by an enemy, you losta point")
+        Player = 1
+    }
+    if (Player > 15) {
+        basic.showString("You got nothing")
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("" + (Points))
@@ -10,7 +30,6 @@ input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     if (Points == 10) {
         basic.showString("You won")
-        basic.showIcon(IconNames.Heart)
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -114,21 +133,48 @@ input.onButtonPressed(Button.B, function () {
             `)
         basic.pause(2000)
         basic.clearScreen()
-    }
-    if (Player > 15) {
-        Points += -1
-        basic.showString("You lost a point")
+    } else if (Player == 11) {
         basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
             . . . . .
-            . # . # .
-            # . . . #
-            . # # # .
             . . . . .
             `)
-        basic.pause(50)
+        basic.pause(2000)
+        basic.clearScreen()
+    } else if (Player == 12) {
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(2000)
+        basic.clearScreen()
+    } else if (Player == 13) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(2000)
+        basic.clearScreen()
+    } else if (Player == 14) {
+        basic.showLeds(`
+            . . . # .
+            . . . # .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(2000)
         basic.clearScreen()
     }
-    if (Player == 10) {
+    if (Player == 15) {
         basic.showString("You got a point")
         Points += 1
     }
@@ -136,8 +182,11 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
-    Player = randint(1, 3)
+    Player = 1
+    Enemy = randint(1, 14)
 })
 let Points = 0
+let Enemy = 0
 let Player = 0
-Player = randint(1, 3)
+Player = 1
+Enemy = randint(1, 14)
